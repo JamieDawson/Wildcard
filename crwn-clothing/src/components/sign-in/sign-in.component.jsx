484bcab7a91958023c62bd15926280bf19df704f
JamieDawson <jamierobertdawson@gmail.com>
2020-07-25 19:23:37 -0700

    updated signin page

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from '../form-iput/form-input.component';
+import CustomButton from '../custom-button/custom-button.component'
 
 import './sign-in.styles.scss';
 
@@ -53,7 +54,7 @@ class SignIn extends React.Component{
                     />
                
 
-                <input type="submit" value="Submit Form"/>
+                <CustomButton type="submit"> Sign In</CustomButton>
             </form>
             </div>
         )
