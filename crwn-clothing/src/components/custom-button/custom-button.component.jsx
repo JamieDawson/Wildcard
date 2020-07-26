9afc492d9d4e38bd6edb0c968b31c1d8fc9a7035
JamieDawson <jamierobertdawson@gmail.com>
2020-07-25 22:36:00 -0700

    updated json to use code editing extentions

--- crwn-clothing/src/components/custom-button/custom-button.component.jsx
+++ crwn-clothing/src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './custom-button.styles.scss'; 
+import './custom-button.styles.scss';
 
-
-const CustomButton = ({ children, ...otherProps}) => ( 
-    <button className='custom-button' {...otherProps}>
-        {children}
-    </button>
+const CustomButton = ({ children, ...otherProps }) => (
+  <button className='custom-button' {...otherProps}>
+    {children}
+  </button>
 );
 
 export default CustomButton;
